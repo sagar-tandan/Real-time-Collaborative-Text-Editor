5178a6c80f4a994c4a515c9c696be08eb00ae018
Sagar Tandan <076bct072@ioepc.edu.np>
2024-11-09 21:32:37 +0545

    Minor changes

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -4,13 +4,14 @@ import mongoose from "mongoose";
 import { AuthRouter } from "./routes/v1/auth";
 import { errorHandler } from "./middleware/errorHandler";
 import morgan from "morgan";
+import http from "http";
 
 dotenv.config();
 const app = express();
+const server = http.createServer(app);
 const port = process.env.PORT || 3000;
 
 app.use(morgan("dev"));
-
 app.use(express.json());
 
 app.use("/api/auth", AuthRouter);
@@ -21,8 +22,9 @@ mongoose
   .connect(process.env.MONGO_CONNECTION_STRING!)
   .then(() => {
     console.log("DB Connected.");
-    app.listen(port, () => {
-      console.log("Server is Listening at port : " + port);
-    });
   })
   .catch((error) => console.error("Error connecting to MongoDB:", error));
+
+server.listen(port, () => {
+  console.log("Server is Listening at port : " + port);
+});
