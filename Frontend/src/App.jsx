b9b1c8e3d5ca81b45c5851bd8a1001030fb4f41a
Sagar Tandan <076bct072@ioepc.edu.np>
2024-11-12 20:44:19 +0545

    React router dom

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,16 +1,25 @@
 import { useState } from "react";
-import Auth from "./components/loginComps/Auth.jsx";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Login from "./components/loginComps/Login.jsx";
+import Register from "./components/Register.jsx";
 
 function App() {
-  const [count, setCount] = useState(0);
 
-  return(
+  return (
+    <Router>
+      <div className="w-full max-w-screen-2xl mx-auto">
+        {/* navigation bar or header here */}
 
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
 
-  <>
-    <Auth />
-  </>
-    )
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
--- Frontend/src/components/loginComps/Login.jsx
+++ Frontend/src/components/loginComps/Login.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const auth = () => {
+const Login = () => {
   return (
     <div className='w-full'>auth</div>
   )
 }
 
-export default auth
+export default Login
