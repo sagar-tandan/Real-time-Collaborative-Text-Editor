81a072057e15ef7385de98263eeb118c47817369
Sagar Tandan <076bct072@ioepc.edu.np>
2024-11-18 21:14:24 +0545

    Streak 2

--- Frontend/src/components/HomePage/HeroSection.jsx
+++ Frontend/src/components/HomePage/HeroSection.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const HeroSection = () => {
   return (
-    <div className='w-full h-screen bg-gradient-to-b from-[#212746] to-[#363a9d] p-3'>HeroSection</div>
+    <div className='w-full h-screen bg-gradient-to-b from-[#212746] to-[#363a9d] p-3'></div>
   )
 }
 
