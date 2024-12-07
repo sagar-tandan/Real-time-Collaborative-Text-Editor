fe26093af174381419d23ecedce963913a3a48e0
Sagar Tandan <076bct072@ioepc.edu.np>
2024-12-07 20:21:59 +0545

    Table Added

--- Frontend/src/components/HomePage/Editor/Editor.jsx
+++ Frontend/src/components/HomePage/Editor/Editor.jsx
@@ -3,6 +3,10 @@ import { useEditor, EditorContent } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import TaskItem from "@tiptap/extension-task-item";
 import TaskList from "@tiptap/extension-task-list";
+import Table from "@tiptap/extension-table";
+import TableCell from "@tiptap/extension-table-cell";
+import TableHeader from "@tiptap/extension-table-header";
+import TableRow from "@tiptap/extension-table-row";
 
 // define your extension array
 const extensions = [
@@ -11,9 +15,11 @@ const extensions = [
   TaskItem.configure({
     nested: true,
   }),
+  Table,
+  TableRow,
+  TableHeader,
+  TableCell,
 ];
-
-const content = "<p>Hello World!</p>";
 
 const Editor = () => {
   const editor = useEditor({
@@ -21,14 +27,29 @@ const Editor = () => {
       attributes: {
         style: "padding-left: 56px; padding-right: 56px; ",
         class:
-          "focus:outline-none bg-white print:border-0 border border-[#C7C7C7] flex flex-col min-h-[1054px] min-w-[816px] py-10 pr-14 cursor-text",
+          "focus:outline-none bg-white print:border-0 border border-[#C7C7C7] flex flex-col min-h-[1054px] w-[816px] py-10 pr-14 cursor-text",
       },
     },
     extensions,
-    content,
+    content: `
+        <table>
+          <tbody>
+            <tr>
+              <th>Name</th>
+              <th colspan="3">Description</th>
+            </tr>
+            <tr>
+              <td>Cyndi Lauper</td>
+              <td>Singer</td>
+              <td>Songwriter</td>
+              <td>Actress</td>
+            </tr>
+          </tbody>
+        </table>
+      `,
   });
   return (
-    <div className="w-full overflow-x-auto bg-[#F9FBFD] px-4 print:p-0 print:overflow-visible print:bg-white">
+    <div className="size-full overflow-x-auto bg-[#F9FBFD] px-4 print:p-0 print:overflow-visible print:bg-white">
       <div className="min-w-max flex justify-center w-[816px] py-4 mx-auto print:py-0 print:w-full print:min-w-0">
         <EditorContent editor={editor} />
       </div>
