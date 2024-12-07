6710a16f1a6f65857af0a26f0d04d19f7fa47c47
Sagar Tandan <076bct072@ioepc.edu.np>
2024-12-07 21:12:39 +0545

    Image Added

--- Frontend/src/components/HomePage/Editor/Editor.jsx
+++ Frontend/src/components/HomePage/Editor/Editor.jsx
@@ -7,6 +7,8 @@ import Table from "@tiptap/extension-table";
 import TableCell from "@tiptap/extension-table-cell";
 import TableHeader from "@tiptap/extension-table-header";
 import TableRow from "@tiptap/extension-table-row";
+import Image from "@tiptap/extension-image";
+import ImageResize from "tiptap-extension-resize-image";
 
 // define your extension array
 const extensions = [
@@ -19,6 +21,8 @@ const extensions = [
   TableRow,
   TableHeader,
   TableCell,
+  Image,
+  ImageResize,
 ];
 
 const Editor = () => {
