a384802842a179c46c8c579ed7bce3bc441f7f13
Sagar Tandan <076bct072@ioepc.edu.np>
2024-12-26 22:23:18 +0545

    Responsive Document Item Updated

---
+++ Frontend/src/components/HomePage/DocumentItem.jsx
@@ -0,0 +1,81 @@
+import React, { useContext } from "react";
+import { TableCell, TableRow } from "../ui/table";
+import {
+  BookIcon,
+  Building2Icon,
+  CircleUserIcon,
+  ExternalLinkIcon,
+  MoreVertical,
+  TrashIcon,
+  Type,
+} from "lucide-react";
+import { SiGoogledocs } from "react-icons/si";
+import { format } from "date-fns";
+import { Button } from "../ui/button";
+import MyContext from "@/Context/MyContext";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { useNavigate } from "react-router-dom";
+
+const DocumentItem = ({ document }) => {
+  const { user } = useContext(MyContext);
+  const navigate = useNavigate();
+  return (
+    <TableRow
+      onClick={() => navigate(`/document/${document.doc_id}`)}
+      className="cursor-pointer hover:bg-neutral-100/80"
+    >
+      <TableCell className="w-[50px]">
+        <SiGoogledocs className="size-6 fill-blue-500" />
+      </TableCell>
+
+      <TableCell className="font-medium md:w-[40%] py-1">
+        {document.doc_title}
+      </TableCell>
+
+      <TableCell className="hidden md:flex items-center gap-2">
+        {document.ownerEmail === user.userEmail ? (
+          <CircleUserIcon className="size-5" />
+        ) : (
+          <Building2Icon className="size-5" />
+        )}
+        {document.ownerEmail === user.userEmail ? "Personal" : "Organization"}
+      </TableCell>
+
+      <TableCell className="hidden md:table-cell py-1">
+        {format(new Date(document.updatedAt), "MMM dd, yyyy")}
+      </TableCell>
+
+      <TableCell className="py-1 flex ml-auto justify-end">
+        <DropdownMenu>
+          <DropdownMenuTrigger
+            onClick={(e) => e.stopPropagation()}
+            className="cursor-pointer"
+          >
+            <MoreVertical className="z-10 size-9 hover:bg-neutral-300/80 rounded-full p-2 text-neutral-800 transition-all ease-in-out duration-300" />
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuItem onClick={(e) => e.stopPropagation()}>
+              <Type className="size-4 mr-2" />
+              Rename
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={(e) => e.stopPropagation()}>
+              <TrashIcon className="size-4 mr-2" />
+              Remove
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={(e) => e.stopPropagation()}>
+              <ExternalLinkIcon className="size-4 mr-2" />
+              Open in new Tab
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </TableCell>
+    </TableRow>
+  );
+};
+
+export default DocumentItem;
--- Frontend/src/components/HomePage/UserDocuments.jsx
+++ Frontend/src/components/HomePage/UserDocuments.jsx
@@ -12,27 +12,28 @@ import React, { useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import { parseISO, format } from "date-fns";
+
 import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "../ui/table";
+import DocumentItem from "./DocumentItem";
 
 const UserDocuments = () => {
-  const navigate = useNavigate();
   const { endPoint, token, user } = useContext(MyContext);
   const [isLoading, setLoading] = useState(true); // Set initial loading to true
   const [allDocuments, setDocuments] = useState([]);
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    // const timeoutId = setTimeout(() => {
     const getAllUserDocuments = async () => {
       if (!user?.userId) {
         setError("User or token not available");
         setLoading(false);
-        // setRefreshKey((prevKey) => prevKey + 1); // This will trigger component reload
         return;
       }
 
@@ -62,15 +63,7 @@ const UserDocuments = () => {
     };
 
     getAllUserDocuments();
-    // }, 1000);
-
-    // return () => clearTimeout(timeoutId);
   }, [user?.userId]);
-
-  const parseDate = (timestamp) => {
-    const date = parseISO(timestamp);
-    return format(date, "MMM dd, yyyy");
-  };
 
   if (isLoading) {
     return (
@@ -89,64 +82,33 @@ const UserDocuments = () => {
   }
 
   return (
-    <div className="max-w-screen-xl mx-auto px-16 py-6 flex flex-col gap-y-4">
-      <div className="flex justify-between">
-        <span className="font-medium">Recent Documents</span>
-        <span className="font-medium">Owned by</span>
-        <span className="font-medium">Last Updated</span>
-        <span className="font-medium w-16"></span>
-      </div>
-      <div className="w-full flex flex-col gap-y-2">
-        {allDocuments.length === 0 ? (
-          <div className="text-center py-4 text-gray-500">
-            No documents found
-          </div>
-        ) : (
-          allDocuments.map((document) => (
-            <div
-              key={document.doc_id}
-              onClick={() => navigate(`/document/${document.doc_id}`)}
-              className="w-full flex justify-between items-center rounded-full py-2 px-6 hover:bg-[#f1f3f4] transition-all duration-300 ease-in-out cursor-pointer"
-            >
-              <div className="flex gap-2 justify-center items-center">
-                <FileText className="size-4 text-blue-600" />
-                <span className="font-medium text-sm text-neutral-800">
-                  {document.doc_title}
-                </span>
-              </div>
+    <div className="max-w-screen-xl mx-auto px-16 py-6 flex flex-col gap-5">
+      <Table>
+        <TableHeader>
+          <TableRow className="hover:bg-transparent border-none">
+            <TableHead>Name</TableHead>
+            <TableHead>&nbsp;</TableHead>
+            <TableHead className="hidden md:table-cell">Shared</TableHead>
+            <TableHead className="hidden md:table-cell">Created at</TableHead>
+          </TableRow>
+        </TableHeader>
 
-              <span className="text-sm text-neutral-800">
-                {document.ownerName}
-              </span>
-              <span className="text-sm text-neutral-800">
-                {parseDate(document.updatedAt)}
-              </span>
-              <DropdownMenu>
-                <DropdownMenuTrigger
-                  onClick={(e) => e.stopPropagation()}
-                  className="cursor-pointer"
-                >
-                  <EllipsisVertical className="z-10 size-9 hover:bg-neutral-300/80 rounded-full p-2 text-neutral-800 transition-all ease-in-out duration-300" />
-                </DropdownMenuTrigger>
-                <DropdownMenuContent>
-                  <DropdownMenuItem onClick={(e) => e.stopPropagation()}>
-                    <Type className="size-4 mr-2" />
-                    Rename
-                  </DropdownMenuItem>
-                  <DropdownMenuItem onClick={(e) => e.stopPropagation()}>
-                    <TrashIcon className="size-4 mr-2" />
-                    Remove
-                  </DropdownMenuItem>
-                  <DropdownMenuItem onClick={(e) => e.stopPropagation()}>
-                    <ExternalLinkIcon className="size-4 mr-2" />
-                    Open in new Tab
-                  </DropdownMenuItem>
-                </DropdownMenuContent>
-              </DropdownMenu>
-            </div>
-          ))
+        {allDocuments.length === 0 ? (
+          <TableBody>
+            <TableRow className="hover:bg-transparent">
+              <TableCell colspan={4} className="h-24 text-center ">
+                No document found
+              </TableCell>
+            </TableRow>
+          </TableBody>
+        ) : (
+          <TableBody>
+            {allDocuments.map((document) => (
+              <DocumentItem key={document.doc_id} document={document} />
+            ))}
+          </TableBody>
         )}
-      </div>
+      </Table>
     </div>
   );
 };
